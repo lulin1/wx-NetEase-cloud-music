e20699e71bba5554ec91235b63abdf93ce4c1fd7
1 <lulin1@xiaomi.com>
2017-09-13 17:01:01 +0800

    Add mockjs & mock-data and weui

---
+++ mock-data.js
@@ -0,0 +1,10 @@
+var Mock = require('mock-min.js')
+export const searchSongs = Mock.mock({
+
+  // 'result|1-10': [{
+  //   // 属性 id 是一个自增数，起始值为 1，每次增 1
+  //   'id|+1': 1
+  // }]
+
+  "result|1-10": { "songs": [{ "name": "大海", "id": 188057, "position": 6, "alias": [], "status": 0, "fee": 8, "copyrightId": 7002, "disc": "1", "no": 6, "artists": [{ "name": "张雨生", "id": 6459, "picId": 0, "img1v1Id": 0, "briefDesc": "", "picUrl": "http://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg", "img1v1Url": "http://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg", "albumSize": 0, "alias": [], "trans": "", "musicSize": 0 }], "album": { "name": "大海", "id": 19049, "type": "专辑", "size": 10, "picId": 106652627906340, "blurPicUrl": "http://p1.music.126.net/rTmIXtO0oWpu4RBT2wYTNg==/106652627906340.jpg", "companyId": 0, "pic": 106652627906340, "picUrl": "http://p1.music.126.net/rTmIXtO0oWpu4RBT2wYTNg==/106652627906340.jpg", "publishTime": 720547200000, "description": "", "tags": "", "company": "飞碟唱片", "briefDesc": "", "artist": { "name": "", "id": 0, "picId": 0, "img1v1Id": 0, "briefDesc": "", "picUrl": "http://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg", "img1v1Url": "http://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg", "albumSize": 0, "alias": [], "trans": "", "musicSize": 0 } } }] } 
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,57 +1,149 @@
 //index.js
 //获取应用实例
-const app = getApp()
+var Mock = require('../../mock-min.js')
+import * as res from '../../mock-data.js';
+console.log(res.searchSongs.result)
+var app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    inputShowed: false,
+    inputVal: "",
+    tabs: ["个性推荐", "歌单", "主播电台", "排行榜"],
+    activeIndex: 0,
+    sliderOffset: 0,
+    sliderLeft: 0,
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 2000,
+    duration: 1000,
+    circular: true,
+    //   歌曲搜索的结果
+    searchReault: []
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  showInput: function () {
+    this.setData({
+      inputShowed: true
+    });
+  },
+  hideInput: function () {
+    this.setData({
+      inputVal: "",
+      inputShowed: false
+    });
+  },
+  clearInput: function () {
+    this.setData({
+      inputVal: ""
+    });
+  },
+  inputTyping: function (e) {
+    let that = this
+    // console.log(e.detail)
+    this.setData({
+      inputVal: e.detail.value
+    });
+    // let url = `http://localhost:3000/search?keywords=${e.detail.value}`
+    // wx.request({
+    //     url: 'http://neteasemusic.leanapp.cn/search',
+    //     data: {
+    //         keywords: e.detail.value
+    //     },
+    //     method: 'GET',
+    //     success: function (res) {
+    //         let temp = []
+    //         if(!res.data.result.songs){
+    //             return ;
+    //         }
+    //         res.data.result.songs.forEach((song, index) => {
+
+    //             temp.push({
+    //                 id: song.id,
+    //                 name: song.name,
+    //                 mp3Url: song.mp3Url,
+    //                 picUrl: song.album.picUrl,
+    //                 singer: song.artists[0].name
+    //             })
+    //             that.setData({
+    //                 searchReault: temp
+    //             })
+
+
+    //         })
+    //         // 存入搜索的结果进缓存
+    //         wx.setStorage({
+    //             key:"searchReault",
+    //             data:temp
+    //         })
+    //     },
+    //     fail: function (res) {
+    //         // fail
+    //     },
+    //     complete: function (res) {
+    //         // complete
+
+    //     }
+    // })
+
+    let temp = []
+    if (!res.searchSongs.result) {
+      return;
+    }
+    res.searchSongs.result.songs.forEach((song, index) => {
+      temp.push({
+        id: song.id,
+        name: song.name,
+        mp3Url: song.mp3Url,
+        picUrl: song.album.picUrl,
+        singer: song.artists[0].name
+      })
+      that.setData({
+        searchReault: temp
+      })
     })
   },
-  ll: function () {
-    console.log(1)
+  onShow: function () {
+    wx.hideLoading()
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+        });
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    });
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  toggleNavBar: function (e) {
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      activeIndex: e.currentTarget.id
+    })
+  },
+  tabClick: function (e) {
+    // console.log(e.currentTarget.id)
+    this.setData({
+      sliderOffset: e.currentTarget.offsetLeft,
+      activeIndex: e.currentTarget.id
+    });
+  },
+  tonow: function (event) {
+    let songData = {
+      id: event.currentTarget.dataset.id,
+      name: event.currentTarget.dataset.name,
+      mp3Url: event.currentTarget.dataset.songurl,
+      picUrl: event.currentTarget.dataset.picurl,
+      singer: event.currentTarget.dataset.singer
+    }
+    // 将当前点击的歌曲保存在缓存中
+    wx.setStorageSync('clickdata', songData)
+    wx.switchTab({
+      url: '../now/index'
     })
   }
-})
+});
